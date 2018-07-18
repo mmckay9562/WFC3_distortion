24ca4a8ee5063df79f791e79c206e41d862088bf
Myles <mmckay@stsci.edu>
2018-07-18 15:59:23 -0400

    Added scripts and juypter notebook

---
+++ wfc3_ir_distortion/ir_residual_plotting.py
@@ -0,0 +1,195 @@
+"""Runs tweakreg and creates plot
+
+This script reads in *fit.match files from tweakreg output and makes .png 
+residual plots for each filter. This code must be ran with python 3 for correct rms. 
+
+
+Author:
+
+--------- Myles McKay, October 20, 2016
+
+Use:
+
+python residual_plotting.py --path='/grp/hst/wfc3v/martlin/Myles_GD_files/medium_filter_test/F410M/old_files' --filter='F####' --new_or_old_version='old'
+python ir_residual_plotting.py --path='./tweakreg_test11' --filter='160W' --new_or_old_version='old'
+
+Output:
+
+Residual plots in png format
+
+Dependencies:
+
+Notes:
+"""
+
+
+import glob
+from stsci.tools import teal
+#from drizzlepac import tweakreg
+import os
+from astropy.table import Table
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+import argparse
+
+def residual_plotting_main(path,filter,new_or_old_version):
+	os.chdir(path)
+	os.system('pwd')               
+	list_of_files=sorted(glob.glob('*fit.match'))
+	image=sorted(glob.glob('*_flt.fits'))
+	
+	for file,im in zip(list_of_files, image): 	
+		with open(file) as f:
+			content= f.readlines(1)
+			content=f.readlines(2)
+			content=f.readlines(3)
+			xRMS=content[0].split()[5]
+			yRMS=content[0].split()[6]
+			xRMS='%.3f' % float(xRMS)
+			yRMS='%.3f' % float(yRMS)	
+			hdu= fits.open(im)
+			print (filter)	
+	
+			fig, [(ax0, ax2), (ax1, ax3)] = plt.subplots(2, 2, figsize=(12, 6),sharex='col',sharey='row')
+			fig.tight_layout(pad=5, w_pad=0.5, h_pad=.625)
+			table=np.loadtxt(file,usecols=(0,1,6,7))
+			stars=sum(1 for _ in table)
+			fig.suptitle('RMS(X)= {}, RMS(Y)= {}, Filter: {},\n #{}'.format(xRMS,yRMS,filter,stars))
+			
+			ylim=[-0.3,0.3]
+		#-----------------------------------------------										
+		######### X1 VS DX PLOT ##############
+		#------------------------------------------------
+			
+		
+			ax0.plot(table[:,0],table[:,2],'o',markersize=1, label='points')
+			ax0.set_ylabel('DX (pixels)')
+			ax0.set_xlim([np.min(table[:,0])-100, np.max(table[:,0])+100])
+			ax0.set_ylim(ylim)
+			ax0.axhline(xmin=1,xmax=0,linewidth=2, color='red')
+			polyfit=np.polyfit(table[:,0],table[:,2],5)
+			sort=np.sort(table[:,0])
+			polyfit_data=((polyfit[0]*(sort**5))+
+			              (polyfit[1]*(sort**4))+
+			              (polyfit[2]*(sort**3))+
+			              (polyfit[3]*(sort**2))+
+			              (polyfit[4]*(sort**1))+
+			              (polyfit[5]))
+			ax0.plot(sort,polyfit_data,color='green', linewidth=3)		
+		
+		#-----------------------------------------------										
+		######## X1 VS DY PLOT ##############
+		#------------------------------------------------
+		
+			ax1.plot(table[:,0],table[:,3],'o',markersize=1)
+			ax1.set_xlabel('X1 (pixels)')
+			ax1.set_ylabel('DY (pixels)')
+			ax1.set_xlim([np.min(table[:,0])-100, np.max(table[:,0])+100])
+			ax1.set_ylim(ylim)
+			ax1.axhline(xmin=1,xmax=0,linewidth=2, color='red')
+			polyfit=np.polyfit(table[:,0],table[:,3],5)
+			sort=np.sort(table[:,0])
+			polyfit_data=((polyfit[0]*(sort**5))+
+			              (polyfit[1]*(sort**4))+
+			              (polyfit[2]*(sort**3))+
+			              (polyfit[3]*(sort**2))+
+			              (polyfit[4]*(sort**1))+
+			              (polyfit[5]))
+			ax1.plot(sort,polyfit_data,color='green', linewidth=3)
+		
+		
+		#-----------------------------------------------										
+		######## Y1 VS DX PLOT ##############
+		#------------------------------------------------										
+												
+												
+			ax2.plot(table[:,1],table[:,2],'o',markersize=1)
+			ax2.axhline(xmin=1,xmax=0,linewidth=2, color='red')
+			ax2.set_xlim([np.min(table[:,1])-100, np.max(table[:,1])+100])
+			ax2.set_ylim(ylim)
+			polyfit=np.polyfit(table[:,1],table[:,2],5)
+			sort=np.sort(table[:,1])
+			polyfit_data=((polyfit[0]*(sort**5))+
+			              (polyfit[1]*(sort**4))+
+			              (polyfit[2]*(sort**3))+
+			              (polyfit[3]*(sort**2))+
+			              (polyfit[4]*(sort**1))+
+			              (polyfit[5]))
+			ax2.plot(sort,polyfit_data,color='green',markersize=1, linewidth=3)
+		
+		
+		
+		#-----------------------------------------------										
+		######## Y1 VS DY PLOT ##############
+		#------------------------------------------------
+												
+												
+			ax3.plot(table[:,1],table[:,3],'o',markersize=1)
+			ax3.set_xlabel('Y1 (pixels)')
+			ax3.axhline(xmin=1,xmax=0,linewidth=2, color='red')
+			ax3.set_xlim([np.min(table[:,1])-100, np.max(table[:,1])+100])
+			ax3.set_ylim(ylim)
+			polyfit=np.polyfit(table[:,1],table[:,3],5)
+			sort=np.sort(table[:,1])
+			polyfit_data=((polyfit[0]*(sort**5))+
+			              (polyfit[1]*(sort**4))+
+			              (polyfit[2]*(sort**3))+
+			              (polyfit[3]*(sort**2))+
+			              (polyfit[4]*(sort**1))+
+			              (polyfit[5]))
+			ax3.plot(sort,polyfit_data,color='green', linewidth=3)
+			plt.savefig('{}_{}.png'.format(new_or_old_version,file[0:13]))
+			plt.clf()
+			hdu.close()
+
+		
+#	os.system('mkdir *.fits.match.png /grp/hst/wfc3v/martlin/median_filter_tests/')	
+#	os.system('cp *flc.png /grp/hst/wfc3v/martlin/Myles_GD_files/medium_filter_test/old_resids_plots')	
+		
+# -------------------------------------------------------------------
+# For command line execution
+# -------------------------------------------------------------------
+
+def parse_args():
+    """Parses command line arguments.
+
+    Parameters:
+        nothing
+
+    Returns:
+        args : argparse.Namespace object
+            An argparse object containing all of the added arguments.
+
+    Outputs:
+        nothing
+    """
+
+    #Create help string:
+    path_help = 'Path to the folder with files to run tweakreg.'
+    filter_help = 'The filter that is being used'
+    new_or_old_version_help= 'Are you using old or new idctab, npol and d2im files'
+    # Add arguments:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', '-path', dest = 'path', action = 'store',
+                        type = str, required = True, help = path_help)
+    parser.add_argument('--filter', '-filter', dest = 'filter', action = 'store',
+                        type = str, required = True, help = filter_help)
+    parser.add_argument('--new_or_old_version', '-new_or_old_version', dest = 'new_or_old_version', action = 'store',
+                        type = str, required = True, help = new_or_old_version_help)                    
+    # Parse args:
+    args = parser.parse_args()
+
+    return args
+# -------------------------------------------------------------------
+if __name__ == '__main__':
+	args = parse_args()
+
+	residual_plotting_main(args.path, args.filter, args.new_or_old_version)
+
+		
+		
+		
+		
+		
+		
---
+++ wfc3_uvis_distortion/mytweakreg.py
@@ -0,0 +1,101 @@
+"""Runs tweakreg and creates plot
+
+This script sets tweakreg prameter and outputs png 
+residual plots for each filter.
+
+
+Author:
+
+--------- Myles McKay, October 20, 2016
+
+Use:
+
+python mytweakreg.py --path='/grp/hst/wfc3v/martlin/nov_4_2016_Myles' --refim='filename.foo' --filter='F410M'
+
+
+Output:
+
+Residual plots in png format
+
+Dependencies:
+
+Notes:
+"""
+
+
+import glob
+from stsci.tools import teal
+from drizzlepac import tweakreg
+import os
+from astropy.table import Table
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+		
+import argparse
+
+def mytweakreg_main(path, refim):
+
+
+	os.chdir(path)
+	os.system('pwd')
+	updatewcs.updatewcs('*flt.fits')
+	
+
+	teal.unlearn('tweakreg')
+	findpars = {'computesig':True,'threshold':100.,'conv_width':3.5,'peakmin':100.,'peakmax':90000.,'nsigma':1.5,
+					'ratio':1.0, 'theta':0.0,'dqbits':None, 'use_sharp_round':False }
+	
+	tweakreg.TweakReg(glob.glob('*flc.fits'), expand_refcat=False, enforce_user_order=False,
+	                    updatehdr=False, shiftfile=True, writecat=True, clean=False, interactive= True, verbose=False,
+	                    headerlet=False, minobj=15, searchrad=250.0, searchunits='pixels', use2dhist=True, see2dplot=True,
+	                    separation=0.5, fitgeometry= 'rscale', residplot='both', labelsize=8,nclip=5, sigma=3.0,
+	                    refimage=refim,
+	                    imagefindcfg=findpars, 
+	                    refimagefindcfg=findpars)
+	  
+		
+# -------------------------------------------------------------------
+# For command line execution
+# -------------------------------------------------------------------
+
+def parse_args():
+    """Parses command line arguments.
+
+    Parameters:
+        nothing
+
+    Returns:
+        args : argparse.Namespace object
+            An argparse object containing all of the added arguments.
+
+    Outputs:
+        nothing
+    """
+
+    #Create help string:
+    path_help = 'Path to the folder with files to run tweakreg.'
+    refim_help = 'Reference Image for tweakreg'
+    
+    # Add arguments:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', '-path', dest = 'path', action = 'store',
+                        type = str, required = True, help = path_help)
+    parser.add_argument('--refim', '-refim', dest = 'refim', action = 'store',
+                        type = str, required = True, help = refim_help)
+    # Parse args:
+    args = parser.parse_args()
+
+    return args
+# -------------------------------------------------------------------
+if __name__ == '__main__':
+	args = parse_args()
+
+	mytweakreg_main(args.path, args.refim)
+
+		
+		
+		
+		
+		
+		
